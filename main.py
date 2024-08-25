9ddc8949c8cd775a4799ac26ea30239ae2ff7b0e
arashfahim <47042726+arashfahim@users.noreply.github.com>
2024-08-25 11:53:23 -0400

    New run

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.optim as optim
+import numpy as np # type: ignore
+import torch # type: ignore
+import torch.nn as nn # type: ignore
+import torch.optim as optim # type: ignore
 torch.set_default_dtype(torch.float64)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -19,7 +19,7 @@ import equation as eqn
 # import visuals
 import time
 
-from absl import app
+from absl import app # type: ignore
 import pickle as pk
 import os
 import json
@@ -31,25 +31,25 @@ import json
 
 def main(argv):
     del argv
-    pde_params={'dim':5,
-                'kappa':[0.,1.,0.8,0.6,0.4,0.5,0.3,0.2,0.1,0.7], # The first kappa=0 because the drift of wealth process is zero
-                'theta':[0.,0.1,0.2,0.3,0.4,0.5,0.4,0.3,0.2,0.1],
-                'nu':[0.02,0.015,0.11,0.12,0.01,0.013,0.14,0.14,0.01], #Hung's params
-                'nu':[0.2,0.15,0.11,0.12,0.1,0.13,0.14,0.14,0.1],# we do not like vanishing diffusion coefficient
+    pde_params={'dim':3,
+                'kappa':[0.,1.,0.8,0.6,0.4,0.5,0.3,0.2,0.1,0.7,1.,0.8,0.6,0.4,0.5,0.3,0.2,0.1,0.7,1.,0.8,0.6,0.4,0.5,0.3,0.2,0.1,0.7], # The first kappa=0 because the drift of wealth process is zero
+                'theta':[0.,0.1,0.2,0.3,0.4,0.5,0.4,0.3,0.2,0.1,0.1,0.2,0.3,0.4,0.5,0.4,0.3,0.2,0.1,0.1,0.2,0.3,0.4,0.5,0.4,0.3,0.2,0.1],
+                # 'nu':[0.02,0.015,0.11,0.12,0.01,0.013,0.14,0.14,0.01,], #Hung's params
+                'nu':[0.2,0.15,0.11,0.12,0.1,0.13,0.14,0.14,0.1,0.2,0.15,0.11,0.12,0.1,0.13,0.14,0.14,0.1,0.2,0.15,0.11,0.12,0.1,0.13,0.14,0.14,0.1],# we do not like vanishing diffusion coefficient
                 # 'lb':[0.,0.15,0.11,0.12,0.13,0.15,0.11,0.12,0.13,0.15],  # Hung's params
-                'lb':[0.,1.15,1.11,0.12,0.13,0.15,0.11,0.12,0.13,0.15], # New params Make closed form solution more sensitive to time
-                'rho':[0.,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+                'lb':[0.,1.15,1.11,0.12,0.13,0.15,0.11,0.12,0.13,0.15,1.15,1.11,0.12,0.13,0.15,0.11,0.12,0.13,0.15,1.15,1.11,0.12,0.13,0.15,0.11,0.12,0.13,0.15], # New params Make closed form solution more sensitive to time
+                'rho':[0.,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
                 'eta':1.,
                 'T': 1.,#torch.tensor([1.]).to(device),
         }
     t0 = time.time()
-    num_samples = 2**16
-    num_time_intervals = 30
+    num_samples = 2**12
+    num_time_intervals = 10
     max_dim = 10
     size = num_samples* max_dim * num_time_intervals
     iid = torch.randn(size=[size]).to(device)
     print("It takes {:.0f} ms to generate {:,} iid samples.".format(round(1000*(time.time()-t0),6),size))
-    sim_params={'num_samples':2**8,
+    sim_params={'num_samples':2**10,
           'num_time_intervals': 10,
           'iid':iid,
           'start' : 0.0,  
@@ -58,8 +58,8 @@ def main(argv):
           }
     
     
-    num_ite = 20
-    bounds = [1e-1 for n in range(1,int(num_ite/2))] + [n0.25/n for n in range(int(num_ite/2),num_ite+1)]# bounds
+    num_ite = 10
+    bounds = [1. for n in range(num_ite+1)]# bounds
     
     path = os.path.dirname(__file__)
     
@@ -78,7 +78,7 @@ def main(argv):
                                     }
 
     m = cf.OU_drift_semi(pde_params)
-    rand_diff = torch.tensor([1.35])
+    rand_diff = torch.tensor([1.7])
     semi_diff = cf.custom_diff(pde_params,rand_diff)
     k = cf.zero_discount(pde_params)
     g = cf.exponential_terminal(pde_params)
@@ -91,95 +91,42 @@ def main(argv):
     semi = eqn.semilinear(semi_diff,m,F,k,g,pde_params,sim_params)
 
     
-    test1 = semi_diff
+    sigma = semi_diff
     for i in range(sim_params['num_time_intervals']):
         if i == 0:
-            t = test1(semi.x[:,:,i]).squeeze(-1)
+            t = sigma(semi.x[:,:,i]).squeeze(-1)
         else:
-            t = torch.cat((t,test1(semi.x[:,:,i]).squeeze(-1)),axis=0)
+            t = torch.cat((t,sigma(semi.x[:,:,i]).squeeze(-1)),axis=0)
     output_dict[0] = {}
-    output_dict[0]['ell'] = {'min':t[:,0,0].min().clone().detach().numpy().item(),
+    output_dict[0]['ell'] = {'min':0.,
+                            'mean':0.,
+                            'median':0.,
+                            'max':0.,
+                            'std':0.
+                            }
+    output_dict[0]['sigma'] = {'min':t[:,0,0].min().clone().detach().numpy().item(),
                             'mean':t[:,0,0].mean().clone().detach().numpy().item(),
                             'median':t[:,0,0].median().clone().detach().numpy().item(),
                             'max':t[:,0,0].max().clone().detach().numpy().item(),
                             'std':t[:,0,0].std().clone().detach().numpy().item()
                             }
-    print(output_dict[0])
     
     print("semi 1")
     semi.train(lr=1e-2,delta_loss=1e-10,max_num_epochs=2500)
     
-    # drift = cf.custom_drift(pde_params,semi.Yt,semi_diff)
-    ell = cf.direction(pde_params,semi.Yt,semi_diff, bound = bounds[0])
-    # source = cf.source_from_direction(pde_params,ell)
-    # g0 = cf.zero_terminal(pde_params)
-    
-    # lin = eqn.linear(semi_diff,drift,source,k,g0,pde_params,sim_params)
 
-    test1 = semi_diff + ell
-    for i in range(sim_params['num_time_intervals']):
-        if i == 0:
-            t = test1(semi.x[:,:,i]).squeeze(-1)
-        else:
-            t = torch.cat((t,test1(semi.x[:,:,i]).squeeze(-1)),axis=0)
-    output_dict[1] = {}
-    output_dict[1]['ell'] = {'min':t[:,0,0].min().clone().detach().numpy().item(),
-                            'mean':t[:,0,0].mean().clone().detach().numpy().item(),
-                            'median':t[:,0,0].median().clone().detach().numpy().item(),
-                            'max':t[:,0,0].max().clone().detach().numpy().item(),
-                            'std':t[:,0,0].std().clone().detach().numpy().item()
-                            }
-    # print("lin 1")
-    # lin.train(lr=1e-2,delta_loss=1e-9,max_num_epochs=2500)
-    
-    
-    # ell1 = ell*lin.Yt
-    # test1 = semi_diff + ell1
-    # for i in range(sim_params['num_time_intervals']):
-    #     if i == 0:
-    #         t = test1(lin.x[:,:,i]).squeeze(-1)
-    #     else:
-    #         t = torch.cat((t,test1(lin.x[:,:,i]).squeeze(-1)),axis=0)
-    # output_dict[1]['nabla u * ell'] = {'min':t[:,0,0].min().clone().detach().numpy().item(),
-    #                                    'mean':t[:,0,0].mean().clone().detach().numpy().item(),
-    #                                    'median':t[:,0,0].median().clone().detach().numpy().item(),
-    #                                    'max':t[:,0,0].max().clone().detach().numpy().item(),
-    #                                    'std':t[:,0,0].std().clone().detach().numpy().item()
-    #                                    }
-    
-    # ell2 = cf.direction(pde_params,semi.Yt,semi_diff, bound = lin.Yt)
-    # test1 = semi_diff + ell2
-    # for i in range(sim_params['num_time_intervals']):
-    #     if i == 0:
-    #         t = test1(lin.x[:,:,i]).squeeze(-1)
-    #     else:
-    #         t = torch.cat((t,test1(lin.x[:,:,i]).squeeze(-1)),axis=0)
-    # output_dict[1]['min(nabla u , ell)'] = {'min':t[:,0,0].min().clone().detach().numpy().item(),
-    #                                    'mean':t[:,0,0].mean().clone().detach().numpy().item(),
-    #                                    'median':t[:,0,0].median().clone().detach().numpy().item(),
-    #                                    'max':t[:,0,0].max().clone().detach().numpy().item(),
-    #                                    'std':t[:,0,0].std().clone().detach().numpy().item()
-    #                                    }
-    print(output_dict[1])
+    with open(file+".json", "w") as outfile: 
+        json.dump(output_dict, outfile) 
 
 
-    for j in range(2,num_ite+1):
-        semi_diff = semi_diff + ell
-        semi = eqn.semilinear(semi_diff,m,F,k,g,pde_params,sim_params)
-        print("semi "+str(j))
-        semi.train(lr=1e-2,delta_loss=1e-10,max_num_epochs=2500)
-        
-        # drift = cf.custom_drift(pde_params,semi.Yt,semi_diff)
+    for j in range(1,num_ite+1):
         ell = cf.direction(pde_params,semi.Yt,semi_diff, bound = bounds[j-1])
-        # source = cf.source_from_direction(pde_params,ell)
-        # lin = eqn.linear(semi_diff,drift,source,k,g0,pde_params,sim_params)
-        
-        test1 = semi_diff + ell
+    
         for i in range(sim_params['num_time_intervals']):
             if i == 0:
-                t = test1(semi.x[:,:,i]).squeeze(-1)
+                t = ell(semi.x[:,:,i]).squeeze(-1)
             else:
-                t = torch.cat((t,test1(semi.x[:,:,i]).squeeze(-1)),axis=0)
+                t = torch.cat((t,ell(semi.x[:,:,i]).squeeze(-1)),axis=0)
         output_dict[j] = {}
         output_dict[j]['ell'] = {'min':t[:,0,0].min().clone().detach().numpy().item(),
                                 'mean':t[:,0,0].mean().clone().detach().numpy().item(),
@@ -187,36 +134,33 @@ def main(argv):
                                 'max':t[:,0,0].max().clone().detach().numpy().item(),
                                 'std':t[:,0,0].std().clone().detach().numpy().item()
                                 }
-        # print("Lin "+str(j))    
-        # lin.train(lr=1e-2,delta_loss=1e-9,max_num_epochs=2500)
-
-        # ell1 = ell*lin.Yt
-        # test1 = semi_diff + ell1
-        # for i in range(sim_params['num_time_intervals']):
-        #     if i == 0:
-        #         t = test1(semi.x[:,:,i]).squeeze(-1)
-        #     else:
-        #         t = torch.cat((t,test1(semi.x[:,:,i]).squeeze(-1)),axis=0)
-        # output_dict[j]['nabla u * ell'] = {'min':t[:,0,0].min().clone().detach().numpy().item(),
-        #                                     'mean':t[:,0,0].mean().clone().detach().numpy().item(),
-        #                                     'median':t[:,0,0].median().clone().detach().numpy().item(),
-        #                                     'max':t[:,0,0].max().clone().detach().numpy().item(),
-        #                                     'std':t[:,0,0].std().clone().detach().numpy().item()
-        #                                     }
-        # ell2 = cf.direction(pde_params,semi.Yt,semi_diff, bound = lin.Yt)
-        # test1 = semi_diff + ell2
-        # for i in range(sim_params['num_time_intervals']):
-        #     if i == 0:
-        #         t = test1(lin.x[:,:,i]).squeeze(-1)
-        #     else:
-        #         t = torch.cat((t,test1(lin.x[:,:,i]).squeeze(-1)),axis=0)
-        # output_dict[j]['min(nabla u , ell)'] = {'min':t[:,0,0].min().clone().detach().numpy().item(),
-        #                                 'mean':t[:,0,0].mean().clone().detach().numpy().item(),
-        #                                 'median':t[:,0,0].median().clone().detach().numpy().item(),
-        #                                 'max':t[:,0,0].max().clone().detach().numpy().item(),
-        #                                 'std':t[:,0,0].std().clone().detach().numpy().item()
-        #                                 }
         print(output_dict[j])
+        
+        with open(file+".json", "w") as outfile: 
+            json.dump(output_dict, outfile) 
+            
+            
+        semi_diff = semi_diff + ell
+        sigma = semi_diff
+        for i in range(sim_params['num_time_intervals']):
+            if i == 0:
+                t = sigma(semi.x[:,:,i]).squeeze(-1)
+            else:
+                t = torch.cat((t,sigma(semi.x[:,:,i]).squeeze(-1)),axis=0)
+        output_dict[j]['sigma'] = {'min':t[:,0,0].min().clone().detach().numpy().item(),
+                                'mean':t[:,0,0].mean().clone().detach().numpy().item(),
+                                'median':t[:,0,0].median().clone().detach().numpy().item(),
+                                'max':t[:,0,0].max().clone().detach().numpy().item(),
+                                'std':t[:,0,0].std().clone().detach().numpy().item()
+                                }
+        print(output_dict[j])
+
+        semi = eqn.semilinear(semi_diff,m,F,k,g,pde_params,sim_params)
+        print("semi "+str(j+1))
+        semi.train(lr=1e-2,delta_loss=1e-10,max_num_epochs=2500)
+        
+        ell = cf.direction(pde_params,semi.Yt,semi_diff, bound = bounds[j-1])
+    
         
         with open(file+".json", "w") as outfile: 
             json.dump(output_dict, outfile) 
