58bb202adb971d7b0b76cffce5aa5098de26ac4c
arashfahim <47042726+arashfahim@users.noreply.github.com>
2024-08-27 10:14:50 -0400

    10 d run at office

--- samplepaths.py
+++ samplepaths.py
@@ -9,16 +9,17 @@ class data_gen(object):
         a = torch.tensor(sim['start']).to(device)
         b = torch.tensor(sim['end']).to(device)
         iid = sim['iid']
-        num_samples = sim['num_samples']
+        self.num_samples = sim['num_samples']
         n = sim['num_time_intervals']
-        dim = pde['dim']
+        self.dim = pde['dim']
         self.dt = torch.tensor([pde['T']/n]).to(device)
-        self.dw = iid[0:num_samples*dim*n].reshape([num_samples,dim,n]).to(device)* torch.sqrt(self.dt)#you can make randomness a universal variable if 
-        self.x = torch.zeros((num_samples,dim+1,n+1))
+        self.dw = iid[0:self.num_samples*self.dim*n].reshape([self.num_samples,self.dim,n]).to(device)* torch.sqrt(self.dt)#you can make randomness a universal variable if 
+        self.x = torch.zeros((self.num_samples,self.dim+1,n+1))
         
-        self.x[:,1:,0]= a+(b-a)*torch.rand(num_samples,dim).to(device)  
-        self.sigmadw = torch.zeros((num_samples,dim,n)).to(device)
+        
+        self.x[:,1:,0]= a+(b-a)*torch.rand(self.num_samples,self.dim).to(device)  
+        self.sigmadw = torch.zeros((self.num_samples,self.dim,n)).to(device)
         for i in range(n):
-            self.sigmadw[:,:,i] = torch.bmm(self.sigma(self.x[:,:,i]).reshape((num_samples,dim,dim)),self.dw[:, :, i].unsqueeze(2)).squeeze(2)
+            self.sigmadw[:,:,i] = torch.bmm(self.sigma(self.x[:,:,i]).reshape((self.num_samples,self.dim,self.dim)),self.dw[:, :, i].unsqueeze(2)).squeeze(2)
             self.x[:,1:,i+1] = self.x[:,1:,i] + self.mu(self.x[:,:,i])*self.dt + self.sigmadw[:,:,i]
             self.x[:,0,i+1] = self.x[:,0,i]+self.dt
